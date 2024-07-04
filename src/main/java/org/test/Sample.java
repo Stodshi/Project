6d54f43ef2b33ab24c3eb9a0318f0eb6503713ae
Stodshi <stodshij2@gmail.com>
2024-07-04 20:30:18 +0530

    Stodshi's code

--- src/main/java/org/test/Sample.java
+++ src/main/java/org/test/Sample.java
@@ -4,7 +4,9 @@ public class Sample {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Test");
+		System.out.println("Test1");
+		System.out.println("Test2");
+		System.out.println("Test3");
 		
 		
 		
